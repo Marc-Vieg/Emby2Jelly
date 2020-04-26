0e34ead0333b44bc4ab2806e508fcf4603c2782c
CobayeGunther <cobayegunter@tuta.io>
2020-04-26 12:11:53 +0200

    whoopsie, correct removing of used var

--- APImain.py
+++ APImain.py
@@ -29,7 +29,10 @@ MigrationMedia['JellyId']=''
 MigrationMedia['Name']=''
 MigrationMedia['ProviderIds']={}
 '''
-
+						
+						
+						
+LocalCache = {}
 
 jellyUserDb = {}
 def getConfig(path, section, option, type):
